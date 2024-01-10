b3fd993b2cf0ee4e09e771e8708b63b9e130b646
Jonathan Dobson <fromjdobson@gmail.com>
2024-01-10 22:59:25 +0000

    this adds animation shimmer to the avatar placeholder used when loading

---
+++ components/Navbar/AvatarSkeleton.tsx
@@ -0,0 +1,11 @@
+export default function AvatarSkeleton() {
+  return (
+    <div className='skeleton' style={{
+      width: 40,
+      height: 40,
+      backgroundColor: 'gray',
+      borderRadius: '50%',
+    }}>
+    </div>
+  )
+}
