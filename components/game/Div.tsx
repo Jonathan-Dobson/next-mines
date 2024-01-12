5bda25d84a50ba656b479784bbf28b08443bcb72
Jonathan Dobson <fromjdobson@gmail.com>
2024-01-12 15:38:21 +0000

    address spreading props issue

--- components/game/Div.tsx
+++ components/game/Div.tsx
@@ -2,10 +2,10 @@
 import React from 'react'
 import classesFrom from './functions/classesFromProps'
 
-export default function classesFromProps(props) {
+export default function classesFromProps(props: any) {
   const { children, style, onClick, onRightClick } = props
   return (
-    <div {...style} className={classesFrom(props)}
+    <div style={style} className={classesFrom(props)}
       onClick={onClick ? onClick : undefined}
       onContextMenu={onRightClick ? onRightClick : (e) => { e.preventDefault() }}
     >{children}
