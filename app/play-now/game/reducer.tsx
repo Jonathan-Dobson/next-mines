885634f7da49b6ab5b2ae0edafa9fa2c3783be37
Jonathan Dobson <fromjdobson@gmail.com>
2024-01-17 06:47:09 +0000

    clean up reducer make it DRYer

--- app/play-now/game/reducer.tsx
+++ app/play-now/game/reducer.tsx
@@ -8,7 +8,7 @@ function reducer(state: GameStateType, action: ActionType): GameStateType {
   let minefield = [...state.minefield]
   let gameStatus = state.gameStatus
   let unopenedMines = state.unopenedMines
-  const max: [number, number] = [state.rowSize, state.colSize]
+  const size: [number, number] = [state.rowSize, state.colSize]
   const rownum = action.payload?.position?.rownum
   const colnum = action.payload?.position?.colnum
 
@@ -33,42 +33,35 @@ function reducer(state: GameStateType, action: ActionType): GameStateType {
 
   function openIt(row: number, col: number) {
     minefield[row][col].cellState = 'open'
-    const neighbors = getNeighbors(row, col, max)
-    let queue: [number | undefined, number | undefined][] = []
+    const neighbors = getNeighbors(row, col, size)
+    var queue: [number, number][] = []
 
     minefield[row][col].cellState = neighbors.reduce((acc: number, [r, c]: [number, number]) => {
-      if (minefield[r][c].cellState === 'closed') queue.push([r, c])
+      if (minefield[r][c].cellState === 'closed') {
+        queue = queue.length && typeof queue[0][0] === 'number' ? [...queue, [r, c]] : [[r, c]]
+      }
       return minefield[r][c].hasMine ? acc + 1 : acc
     }, 0)
 
     if (minefield[row][col].cellState === 0 && queue && queue.length > 0) {
-      queue.forEach(([r, c]: [number | undefined, number | undefined]) => {
-        if (typeof r === 'undefined' || typeof c === 'undefined') return
+      queue.forEach(([r, c]: [number, number]) => {
         openIt(r, c)
       })
     }
   }
 
   function checkForWin(): boolean {
-    const allMinesList = minefield.flat()
-      .filter((cell: CellType) => cell.hasMine)
+    const allMinesAreFlagged = minefield.flat()
+      .filter((cell) => cell.hasMine)
+      .every((cell) => cell.cellState === 'flag')
 
-    const allMinesAreFlagged = allMinesList
-      .every((cell: CellType) => cell.cellState === 'flag')
+    const allFlagsHaveMines = minefield.flat()
+      .filter((cell) => cell.cellState === 'flag')
+      .every((cell) => cell.hasMine)
 
-    const allFlags = minefield.flat()
-      .filter((cell: CellType) => cell.cellState === 'flag')
-
-    const allFlagsHaveMines = allFlags
-      .every((cell: CellType) => cell.hasMine)
-
-    const allNonMinesList = minefield.flat()
-      .filter((cell: CellType) => !cell.hasMine)
-
-    const allNonMinesOpen = allNonMinesList
-      .every((cell: CellType) => cell.cellState !== 'closed')
-
-
+    const allNonMinesOpen = minefield.flat()
+      .filter((cell) => !cell.hasMine)
+      .every((cell) => cell.cellState !== 'closed')
 
     return (
       allMinesAreFlagged && allNonMinesOpen && allFlagsHaveMines
@@ -117,7 +110,7 @@ function reducer(state: GameStateType, action: ActionType): GameStateType {
     }
 
     case 'CLICKED_ON_OPEN_CELL': {
-      const neighbors = getNeighbors(rownum, colnum, max)
+      const neighbors = getNeighbors(rownum, colnum, size)
       let flaggedNeighbors = 0
       neighbors.forEach(([r, c]: [number, number]) => {
         if (minefield[r][c]?.cellState === 'flag') {
@@ -135,7 +128,7 @@ function reducer(state: GameStateType, action: ActionType): GameStateType {
     }
   }
   unopenedMines = minefield.flat()
-    .filter((cell: CellType) => cell.hasMine && cell.cellState !== 'flag').length
+    .filter((cell) => cell.hasMine && cell.cellState !== 'flag').length
   return { ...state, unopenedMines }
 }
 
